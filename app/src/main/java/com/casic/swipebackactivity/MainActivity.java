3584e698ff598f60fa8731180488f865caadacaa
guobao <guobao0730@163.com>
2019-04-20 15:52:27 +0800

    init

---
+++ app/src/main/java/com/casic/swipebackactivity/MainActivity.java
@@ -0,0 +1,12 @@
+package com.casic.swipebackactivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
