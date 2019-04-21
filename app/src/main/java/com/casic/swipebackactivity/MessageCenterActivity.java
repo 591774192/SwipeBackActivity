35ca9935cd1d2874e8e7dfca209d56f36272f1eb
guobao <guobao0730@163.com>
2019-04-21 10:50:09 +0800

    新增加了一个消息中心页面来测试右滑退出的效果

---
+++ app/src/main/java/com/casic/swipebackactivity/MessageCenterActivity.java
@@ -0,0 +1,20 @@
+package com.casic.swipebackactivity;
+
+import android.os.Bundle;
+
+/**
+ * @author 郭宝
+ * @project： SwipeBackActivity
+ * @package： com.casic.swipebackactivity
+ * @date： 2019/4/21 0021 10:41
+ * @brief: 消息中心页面
+ */
+public class MessageCenterActivity extends SwipeBackActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_message_center);
+    }
+
+}
