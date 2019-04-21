15c1928a088ac1ea03160cce44f1440ac44e850d
guobao <guobao0730@163.com>
2019-04-21 10:48:38 +0800

    新增加了一个消息中心页面来测试右滑退出的效果

--- app/src/main/java/com/casic/swipebackactivity/MainActivity.java
+++ app/src/main/java/com/casic/swipebackactivity/MainActivity.java
@@ -1,6 +1,9 @@
 package com.casic.swipebackactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends SwipeBackActivity {
 
@@ -8,5 +11,19 @@ public class MainActivity extends SwipeBackActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+
+        Button btn_startActivity = findViewById(R.id.btn_startActivity);
+        btn_startActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,MessageCenterActivity.class));
+            }
+        });
+
     }
 }
